0e9c24cbcd98886c3efac008da208ff0f7447d38
shry <shry@users.noreply.github.com>
2019-05-09 10:23:05 -0300

    Add WP_DEBUG

--- command.php
+++ command.php
@@ -274,7 +274,7 @@ class Wordup_Commands {
                     --skip-email');
         
         //Because its a development context, we set debug to true
-        WP_CLI::runcommand("config set set WP_DEBUG true --raw");
+        WP_CLI::runcommand("config set WP_DEBUG true --raw");
 
         // Only set the flexible port siteurl config, if there is no custom site_url
         if(empty($this->site_url)){
@@ -342,7 +342,7 @@ class Wordup_Commands {
         WP_CLI::runcommand('config set DB_PASSWORD wordpress');
         WP_CLI::runcommand('config set DB_HOST db:3306');
         //Because its a development context, we set debug to true
-        WP_CLI::runcommand("config set set WP_DEBUG true --raw");
+        WP_CLI::runcommand("config set WP_DEBUG true --raw");
 
         if(empty($this->site_url)){
             WP_CLI::runcommand("config set WP_HOME \'".$this->server.":\'.getenv\(\'WORDUP_PORT\'\) --raw");
@@ -433,7 +433,7 @@ class Wordup_Commands {
 
             WP_CLI::runcommand('config create --dbname=wordpress --dbuser=wordpress --dbpass=wordpress --dbhost=db:3306 --force');
             //Because its a development context, we set debug to true
-            WP_CLI::runcommand("config set set WP_DEBUG true --raw");
+            WP_CLI::runcommand("config set WP_DEBUG true --raw");
             if(empty($this->site_url)){
                 WP_CLI::runcommand("config set WP_HOME \'".$this->server.":\'.getenv\(\'WORDUP_PORT\'\) --raw");
                 WP_CLI::runcommand("config set WP_SITEURL \'".$this->server.":\'.getenv\(\'WORDUP_PORT\'\) --raw");
